be9e76f0454759930de29383be821a9efadf24c3
V1S10US <46070521+V1S10US@users.noreply.github.com>
2018-12-27 22:42:58 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import config
 import requests
 import json
+from time import sleep
 
 token = config.token
 
@@ -9,7 +10,8 @@ URL = "https://api.telegram.org/bot" + token + '/'
 
 #https://api.telegram.org/bot713670283:AAFpUNdODrvWvkrWvECAyIqfuNAeVH425Do/sendmessage?chat_id=441814052&text
 
-
+global recent_update_id
+recent_update_id = 0 
 
 def get_updates():                                          # builds .json file with data from url request
 
@@ -23,37 +25,58 @@ def get_message():
 
 	data = get_updates()                                    # makes a dict from .json file from get_address
 
-	chat_id = data['result'][-1]['chat']['id']              # 
-                                                            # saves value from dict
-	message_text = data['result'][-1]['message']['text']    #
+	last_item = data['result'][-1]
 
-	message = {'chat_id' : chat_id,
-			   'text' : message_text}
-	return message
+	update_id = last_item['update_id']
 
+	global recent_update_id
+	if recent_update_id != update_id:                       # takes the last update_id and checks for equality with current one
+
+		recent_update_id = update_id
+
+		chat_id = last_item['message']['chat']['id']        # 
+	                                                        # saves value from dict
+		message_text = last_item['message']['text']         #
+
+		username = last_item['message']['from']['username']
+
+		message = {'chat_id' : chat_id,
+				   'text' : message_text,		
+				    'username' : username}
+		return message
+		return None
 
 def send_message(chat_id,text=''):
 	
 	url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
 	requests.get(url)
 
-
-
+def echo(chat_id,text):
+	send_message(chat_id,text)
 
 def main():
+	'''d = get_updates()
+	with open('updates.json', 'w') as file:
 
- 	#d = get_updates()
+		json.dump(d, file, indent = 2, ensure_ascii = False )'''
 
-	#with open('updates.json', 'w') as file:
+	while True:		
+		answer = get_message()
 
-		#json.dump(d, file, indent = 2, ensure_ascii = False )
-answer = get_message()
+		if answer != None:                                                   # main body 
 
-chat_id = answer['chat_id']
-response = answer['text']
-if response == 'Codename?':
 
-	send_message(chat_id,'Test string')
+			chat_id = answer['chat_id']
+			send_message(chat_id,'Здравствуйте ,{} !'.format(answer['username']))
+
+
+		else:
+			continue
+
+		if answer['text'] == 'stop':
+			break
+		sleep(3)                                                             # saves for overloading
+
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,5 +1,30 @@
-token = "713670283:AAFpUNdODrvWvkrWvECAyIqfuNAeVH425Do"
 """chat_id = id from dict 'chat' 
 sendmessage?[chat_id,text]
 text = sending test
+
+authors ={
+
+lRLANDEZ : 445046363
+		  
+V1S10US  : 441814052
+
+		  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
+
+
+token = "713670283:AAFpUNdODrvWvkrWvECAyIqfuNAeVH425Do"
